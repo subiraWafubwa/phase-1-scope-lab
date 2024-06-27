723867092ef5b5739501907a672ac20175ae5646
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-27 12:08:09 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer = undefined;
+const leastFavoriteCustomer = 'Phillip'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase(); 
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Sonfransisco'
+}
